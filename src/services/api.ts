03ee3f351273219c2566f02b252a4d3c8e35f667
Emmanuel <emmanuelscre1@gmail.com>
2025-04-14 23:42:22 +0100

    modification du lien de l'api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8000/api/',
+  baseURL: import.meta.env.VITE_API_URL || 'https://plum-api.onrender.com/api/',
   headers: {
     'Content-Type': 'application/json',
   },
@@ -41,7 +41,7 @@ api.interceptors.response.use(
         
         // Appel pour rafraîchir le token
         const response = await axios.post(
-          `${import.meta.env.VITE_API_URL || 'http://localhost:8000/api/'}auth/token/refresh/`,
+          `${import.meta.env.VITE_API_URL || 'https://plum-api.onrender.com/api/'}auth/token/refresh/`,
           { refresh: refreshToken }
         );
         
